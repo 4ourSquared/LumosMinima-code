664802c750adb9a418974ffb8047f38f5e277414
matnut2 <matteosolda12@gmail.com>
2023-08-15 18:14:28 +0200

    modifica tipo richiesta

--- server/routes/TokenRoutes.ts
+++ server/routes/TokenRoutes.ts
@@ -3,7 +3,7 @@ import TokenSchema from "../schemas/TokenSchema";
 
 const tokenRoutes =  Router();
 
-tokenRoutes.get("/:id", async (req: Request, res: Response) => {
+tokenRoutes.post("/:id", async (req: Request, res: Response) => {
     const id = parseInt(req.params.id, 10);
     console.log(`Segnale ricevuto a api/movimento/token/aree/${id}`);
 
