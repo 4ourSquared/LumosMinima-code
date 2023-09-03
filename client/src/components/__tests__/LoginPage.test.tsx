889bbc864a8a66debef009cd043ed911f5d424f1
EricaCavaliere <erica.cavaliere.euganeoinf@gmail.com>
2023-09-03 18:39:21 +0200

    Test LoginPage
    Copertura dell'88.88%
    Manca controllo alla riga 16, funzione navigate.
    Si potrebbe aggiungere un controllo di quello che viene inserito nei campi Input, per poter forse aumentare anche la code coverage di LoginMechanism.

--- client/src/components/__tests__/LoginPage.test.tsx
+++ client/src/components/__tests__/LoginPage.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import LoginPage from "../LoginPage";
 import { BrowserRouter as Router } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 test("Render della pagina di login", () => {
     render(
@@ -13,7 +14,18 @@ test("Render della pagina di login", () => {
     const usernameInput = screen.getByLabelText("Nome utente");
     const passwordInput = screen.getByLabelText("Password");
 
+    expect(title).toBeInTheDocument();
     expect(usernameInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
 });
 
+test("Click sul pulsante Entra", async () => {
+    render(
+        <Router>
+            <LoginPage />
+        </Router>
+    )
+
+    await userEvent.click(screen.getByRole('button'));
+    //expect(screen.getByText(/Aree illuminate/i)).toBeInTheDocument();
+})
