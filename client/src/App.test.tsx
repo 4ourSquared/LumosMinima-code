a651111193ddc7d15961d3d2a2d31965c9e73bb5
matnut2 <matteosolda12@gmail.com>
2023-09-05 17:56:39 +0200

    Modifica

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-/*
+
 test("Renderizza la pagina principale", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Lumos Minima/i);
-  expect(linkElement).toBeInTheDocument();
 });
-*/
