88674aea4550d1db546437b7cee37dc53301ee6d
nicolasalberti00 <75423237+nicolasalberti00@users.noreply.github.com>
2023-09-13 17:41:34 +0200

    Modifiche ai test e aggiunta check response di AreaTable. Ancora errori in AreaSingleView

--- client/src/components/__tests__/LoginPage.test.tsx
+++ client/src/components/__tests__/LoginPage.test.tsx
@@ -1,58 +1,65 @@
-import { render, screen } from "@testing-library/react";
-import LoginPage from "../LoginPage";
-import { MemoryRouter, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+/* eslint-disable testing-library/no-unnecessary-act */
+import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "../LoginPage";
 
 test("Render della pagina di login", () => {
-    render(
-    <Router>
-        <LoginPage />
-    </Router>
-    );
+  render(
+    <MemoryRouter>
+      <LoginPage />
+    </MemoryRouter>
+  );
 
-    const title = screen.getByText(/Lumos Minima/i);
-    const usernameInput = screen.getByLabelText("Nome utente");
-    const passwordInput = screen.getByLabelText("Password");
+  const title = screen.getByText(/Lumos Minima/i);
+  const usernameInput = screen.getByLabelText("Nome utente");
+  const passwordInput = screen.getByLabelText("Password");
 
-    expect(title).toBeInTheDocument();
-    expect(usernameInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+  expect(usernameInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
 });
 
 test("Click sul pulsante Entra - campi errati", () => {
-    render(
-        <Router>
-            <LoginPage />
-        </Router>
-    );
+  render(
+    <MemoryRouter>
+      <LoginPage />
+    </MemoryRouter>
+  );
 
-    userEvent.click(screen.getByRole('button'));
-})
+  act(() => {
+    userEvent.click(screen.getByRole("button"));
+  });
+});
 
 test("Click sul pulsante Entra - admin", async () => {
-    render(
-        <MemoryRouter initialEntries={["/"]}>
-            <Routes>
-                <Route path="/" element={<LoginPage />}/>
-            </Routes>
-        </MemoryRouter>
-    )
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </MemoryRouter>
+  );
 
-    userEvent.type(screen.getByLabelText("Nome utente"),"admin");
-    userEvent.type(screen.getByLabelText("Password"),"admin");
-    userEvent.click(screen.getByRole('button'));
-})
+  act(() => {
+    userEvent.type(screen.getByLabelText("Nome utente"), "admin");
+    userEvent.type(screen.getByLabelText("Password"), "admin");
+    userEvent.click(screen.getByRole("button"));
+  });
+});
 
 test("Click sul pulsante Entra - manut", async () => {
-    render(
-        <MemoryRouter initialEntries={["/"]}>
-            <Routes>
-                <Route path="/" element={<LoginPage />}/>
-            </Routes>
-        </MemoryRouter>
-    )
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </MemoryRouter>
+  );
 
-    userEvent.type(screen.getByLabelText("Nome utente"),"manut");
-    userEvent.type(screen.getByLabelText("Password"),"manut");
-    userEvent.click(screen.getByRole('button'));
-})
+  act(() => {
+    userEvent.type(screen.getByLabelText("Nome utente"), "manut");
+    userEvent.type(screen.getByLabelText("Password"), "manut");
+    userEvent.click(screen.getByRole("button"));
+  });
+});
