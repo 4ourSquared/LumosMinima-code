a42d38e05b40ca0d9f136bc0e9293fedab786a63
EricaCavaliere <erica.cavaliere.euganeoinf@gmail.com>
2023-09-16 09:29:22 +0200

    Completato test LampGuastiPage

--- client/src/components/__tests__/LampGuastiPage.test.tsx
+++ client/src/components/__tests__/LampGuastiPage.test.tsx
@@ -1,17 +1,40 @@
 import LampGuastiPage from "../LampGuastiPage"
 import {MemoryRouter,Routes,Route} from 'react-router-dom'
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
+import userEvent from "@testing-library/user-event";
+import LoginPage from "../LoginPage";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 test("Render della pagina dei guasti", async () => {
-    
 
+    mockedAxios.get.mockResolvedValue({
+        data: {
+            id: 1,
+            stato: "attivo",
+            lum: 5,
+            luogo: "test",
+            area: 1,
+            guasto: false,
+            mode: "manuale",
+        },
+    })
+    
     render(
         <MemoryRouter initialEntries={["/"]}>
             <Routes>
                 <Route path="/" element={<LampGuastiPage areaId={1}/>} />
+                <Route path="/login" element={<LoginPage />} />
             </Routes>
         </MemoryRouter>
     )
 
-    expect(screen.getByText("Lista degli impianti luminosi guasti")).toBeInTheDocument();
+    await waitFor(async () => {
+        expect(screen.getByText("Lista degli impianti luminosi guasti")).toBeInTheDocument();
+        expect(screen.getByRole('table'));
+        const button = await screen.findByText("Esci");
+        userEvent.click(button);
+    })
 })
