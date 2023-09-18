c8e3e27fba9c0fb2badd8d4e718dbdd3899b804d
matnut2 <matteosolda12@gmail.com>
2023-09-18 18:05:15 +0200

    Aggiunto pulsante per modifica utente

--- client/src/components/Breadcrumb.tsx
+++ client/src/components/Breadcrumb.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate, useOutletContext } from "react-router-dom";
 import useLogoutMechanism from "../auth/LogoutMechanism";
+import { UserData, Role } from "../auth/Authorization";
 
 interface BreadcrumbProps {}
 
@@ -16,6 +17,7 @@ const breadcrumbMap: { [key: string]: string } = {
   "/api/aree/edit/:id": "Modifica Area",
   "/api/aree/:areaId/sensori/edit/:id": "Modifica Sensore",
   "/api/aree/:areaId/lampioni/guasti": "Lista Lampioni Guasti",
+  "/accounting/userList": "Lista Utenti",
 };
 
 /*
@@ -23,6 +25,7 @@ const breadcrumbMap: { [key: string]: string } = {
 */
 
 const Breadcrumb: React.FC<BreadcrumbProps> = () => {
+  const userData = useOutletContext<UserData>();
   const location = useLocation();
   const path = location.pathname;
   const descrizione = Object.keys(breadcrumbMap).find((key) =>
@@ -46,6 +49,12 @@ const Breadcrumb: React.FC<BreadcrumbProps> = () => {
         <li className="breadcrumb-item">
           {descrizione ? breadcrumbMap[descrizione] : "Pagina Sconosciuta"}
         </li>
+        {
+          userData.role === Role.Amministratore &&
+        <Link to="/accounting/userList" type="button" className="btn btn-warning">
+          Modifica Utente
+        </Link>
+        }   
         <button
           onClick={(e) => {
             prepareLogout(e);
