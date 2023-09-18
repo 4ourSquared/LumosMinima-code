12e02504a4f2f5fa0e1c046371e63be52ef0026a
matnut2 <matteosolda12@gmail.com>
2023-09-18 18:06:04 +0200

    Tabella degli utenti

---
+++ client/src/components/UserTable.tsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
+import { useConfirm } from "material-ui-confirm";
+import { UserData, Role } from "../auth/Authorization";
+import UserItem from "../types/UserItem";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export const UserTable: React.FC = () => {
+    const [users, setUsers] = useState<UserItem[]>([]);
+    const userData = useOutletContext<UserData>();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const loadUsers = async () => {
+            try {
+                const response = await axios.get<UserItem[]>(
+                    "http://localhost:5000/accounting/userList"
+                );
+                setUsers(response.data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        loadUsers();
+    }, []);
+
+    return (
+        <>
+        <Header/>
+            <table className="table table-hover align-middle caption-top">
+                <caption>Lista degli Utenti</caption>
+                <thead>
+                    <tr>
+                        <th scope="col">Username</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Privilegio</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody">
+                    {users.map((user) => (
+                        <tr key={user.username}>
+                            <th scope="row">{user.username}</th>
+                            <td>{user.email}</td>
+                            <td>{user.privilege}</td>
+                            <td>
+                                <button
+                                    className="btn btn-outline-primary"
+                                    onClick={() =>
+                                        navigate(
+                                            `/accounting/users/${user.username}`
+                                        )
+                                    }
+                                >
+                                    Modifica
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <Footer/>
+        </>
+    );
+};
+
+export default UserTable;
