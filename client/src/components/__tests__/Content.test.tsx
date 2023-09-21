2a199906fca73d60b22374868ae39759b643c4e6
matnut2 <matteosolda12@gmail.com>
2023-09-21 18:27:06 +0200

    sistemazione test content

--- client/src/components/__tests__/Content.test.tsx
+++ client/src/components/__tests__/Content.test.tsx
@@ -3,12 +3,22 @@ import axios from "axios";
 import { BrowserRouter as Router } from "react-router-dom";
 import '@testing-library/jest-dom'
 import Content from "../Content";
+import { Role } from "../../auth/Authorization";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const mockUserData = { role: Role.Amministratore };
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useOutletContext: () => mockUserData,
+}));
+
 test("Render della pagina 'Content' dove 'AreaTable' è vuota", async () => {
   
+  const mockResponseData = [{ id: 1, nome: "Area 1", descrizione: "Descrizione 1", latitudine: 123, longitudine: 456 }];
+  mockedAxios.get.mockResolvedValue({ data: mockResponseData });
+
 
   render(
     <Router>
